2a75369ca26096c2be0f2be18562e2b7207fc378
RedEyeManiac <jordancookirwin@gmail.com>
2015-01-24 19:39:58 +1100

    credits working

--- UnityProject/Assets/scripts/CreditsScript.cs
+++ UnityProject/Assets/scripts/CreditsScript.cs
@@ -11,7 +11,7 @@ public class CreditsScript : MonoBehaviour {
 		
 		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height),backgroundTexture, ScaleMode.ScaleToFit);
 		
-		if (GUI.Button (new Rect (Screen.width * .25f, Screen.height * .5f, Screen.width * .5f, Screen.height * .1f), "", button1)) {
+		if (GUI.Button (new Rect (Screen.width * .375f, Screen.height * .875f, Screen.width * .25f, Screen.height * .1f), "", button1)) {
 			print ("Clicked Back");
 			Application.LoadLevel ("MainMenu");
 		}
