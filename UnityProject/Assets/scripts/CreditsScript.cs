607cb12e452dfc3553d6a844376779e1ff9bf6fa
RedEyeManiac <jordancookirwin@gmail.com>
2015-01-24 16:39:34 +1100

    credits

--- UnityProject/Assets/scripts/CreditsScript.cs
+++ UnityProject/Assets/scripts/CreditsScript.cs
@@ -1,25 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MainMenuScript : MonoBehaviour {
+public class CreditsScript : MonoBehaviour {
 	
 	public Texture backgroundTexture;
 	
 	public GUIStyle button1;
-	public GUIStyle button2;
 	
 	void OnGUI(){
 		
 		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height),backgroundTexture, ScaleMode.ScaleToFit);
 		
 		if (GUI.Button (new Rect (Screen.width * .25f, Screen.height * .5f, Screen.width * .5f, Screen.height * .1f), "", button1)) {
-			print ("Clicked Play Game");
-			Application.LoadLevel ("Room01");
-		}
-		
-		if (GUI.Button (new Rect (Screen.width * .25f, Screen.height * .5f + (Screen.height * .15f), Screen.width * .5f, Screen.height * .1f), "", button2)) {
-			print ("Clicked Credits");
-			Application.LoadLevel ("Credits");
+			print ("Clicked Back");
+			Application.LoadLevel ("MainMenu");
 		}
 	}
 }
