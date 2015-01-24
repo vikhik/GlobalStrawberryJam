63dd3aef5519d79eaf05de776a3bdfbdd3aff6b7
RedEyeManiac <jordancookirwin@gmail.com>
2015-01-24 16:03:16 +1100

    main menu added

---
+++ UnityProject/Assets/MainMenu/MainMenuScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+	
+	public Texture backgroundTexture;
+	
+	public GUIStyle button1;
+	
+	void OnGUI(){
+		
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height),backgroundTexture, ScaleMode.ScaleToFit);
+		
+		if (GUI.Button (new Rect (Screen.width * .25f, Screen.height * .5f, Screen.width * .5f, Screen.height * .1f), "", button1)) {
+			print ("Clicked Play Game");
+		}
+		
+		if (GUI.Button (new Rect (Screen.width * .25f, Screen.height * .5f + (Screen.height * .15f), Screen.width * .5f, Screen.height * .1f), "", button1)) {
+			print ("Clicked Credits");
+		}
+		
+		if (GUI.Button (new Rect (Screen.width * .25f, Screen.height * .5f + (Screen.height * .3f), Screen.width * .5f, Screen.height * .1f), "", button1)) {
+			print ("Clicked Exit");
+		}
+	}
+}
