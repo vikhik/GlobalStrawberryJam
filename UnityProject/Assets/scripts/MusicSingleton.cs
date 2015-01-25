b524c4c75555a37ddfe30a4a518351dea51427e9
DavidST44 <dthompsoncs@gmail.com>
2015-01-25 14:25:35 +1100

    music + script
    plays across scene loads so theres no skipping

---
+++ UnityProject/Assets/scripts/MusicSingleton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicSingleton : MonoBehaviour {
+	
+	private static MusicSingleton instance = null;
+	
+	public static MusicSingleton Instance {
+		get { return instance; }
+	}
+	
+	void Awake() 
+	{
+		if (instance != null && instance != this) 
+		{
+			audio.Stop();
+			if(instance.audio.clip != audio.clip)
+			{
+				instance.audio.clip = audio.clip;
+				instance.audio.volume = audio.volume;
+				instance.audio.Play();
+			}
+			
+			Destroy(this.gameObject);
+			return;
+		} 
+		instance = this;
+		audio.Play ();
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
